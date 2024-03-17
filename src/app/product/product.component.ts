c72e4fc97a5f47a5afa27b40dc63415fd654ed0c
zahid hasan <zahidhasan19932023@hotmail.com>
2024-03-17 14:32:48 +0600

    two types of card in a single component with condition

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  @Input() view: 'grid' | 'list' = 'list';
   constructor() { }
 
   ngOnInit(): void {
